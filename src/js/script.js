c372e89fc0b86f016a46f2d721ea3d3812852395
Alexey Goryainov <vladimirovich.alex.gor@gmail.com>
2022-10-19 08:52:32 +0200

    Допилена менюшка. Добавлен скрипт к меню

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+
+// Вариант с getElementsByClassName
+// const hamburger = document.getElementsByClassName('hamburger'),
+//     menu = document.getElementsByClassName('menu'),
+//     closeElem = document.getElementsByClassName('menu__close');
+
+// console.log(hamburger.length);
+// console.log(menu.length);
+
+
+// hamburger[0].onclick = function () {
+//     menu[0].classList.add('active');
+// }
+
+// Вариант с querySelector
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+hamburger.onclick = function () {
+    menu.classList.add('active');
+}
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
